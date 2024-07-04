1e05defbf8364dfd404bdc896710092512a1f0d6
Michael Han <mykalehan@gmail.com>
2024-07-04 15:44:48 +1000

    accidentally on main but alg. added nice UI for home page and search bar. Started implementing search results

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav className="navigationBar">
+          <a href="/">Sustainment</a>
+          <a href="/discover">Discover</a>
+          <a href="/about">About</a>
+          <a href="/login">Login</a>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
